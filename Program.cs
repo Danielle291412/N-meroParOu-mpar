08950bb96667b94447cd27c4971d354b7e3f1cde
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-01 20:32:36 -0300

    Exiba se o número digitado é ímpar ou par

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿ExibirTítulo();
+Console.Write("Informe um número:");
+int numero = Convert.ToInt32(Console.ReadLine());
+bool ehPar = DefinirPar(numero);
+
+DefinirPar(numero);
+ExibirResultado();
+
+void ExibirTítulo()
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("----------------------");
+    Console.WriteLine("-----Par ou Ímpar-----");
+    Console.WriteLine("--------------------\n");
+    Console.ResetColor();
+}
+
+bool DefinirPar(int numero)
+{
+    bool ehPar = numero % 2 == 0;
+    return ehPar;
+}
+
+void ExibirResultado()
+{
+    Console.WriteLine($"O número {numero} é {(ehPar ? "Par" : "Ímpar")}");
+}
